75e34e44f201700f0eff35786322b8c698d94959
Thomas Ung <tomkimsour@gmail.com>
2023-05-29 15:59:33 +0930

    Gpsr test descr (#3)
    * add gpsr test changes
    
    * correted include file robobreizh_msgs
    
    * remove message generation
    
    * commit gpsr-test
    
    * correct messages import
    
    * remove unecessary cmake command
    
    * remove shitty runtime message
    
    * remove test sentence
    
    * remove sentence
    
    * remove sentence
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * speedy speed boi
    
    * speeeeeeeeed
    
    * add descr
    
    * add ast
    
    * less speedy
    
    * fine grained descr
    
    ---------
    
    Co-authored-by: Rebeccalaylay <rebeccalay20@hotmail.com>
    Co-authored-by: Robobreizh pepper <>
    Co-authored-by: SinuoWang <1145720231@qq.com>
    Co-authored-by: khksamuel <a1821027@student.adelaide.edu.au>

--- scripts/TranscriptIntent.py
+++ scripts/TranscriptIntent.py
@@ -1,4 +1,4 @@
-#!/user/bin/env python
+#!/user/bin python3
 import rospy
 
 from dialog_utils.utils import *
@@ -7,8 +7,9 @@ from robobreizh_msgs.srv import *
 # from dialog_pepper.srv import *
 from predict_robot import CommandProcessor
 import spacy
-
-
+import json 
+import re
+import ast
 
 class Intent():
     def __init__(self):
@@ -33,6 +34,83 @@ class Intent():
             # req.transcript = 'Find the person wearing a red shirt' #####################
             # -------------------------
             parser_intent = self.parser.predict(req.transcript.replace(", "," , ").split())
+            print(parser_intent)
+
+
+            parser_intent = re.sub("''","'",parser_intent)
+            begin_lst = [m.start() for m in re.finditer('{',parser_intent)]
+            end_lst = [m.start() for m in re.finditer('}',parser_intent)]
+
+            task_lst = [parser_intent[b:e+1] for b,e in zip(begin_lst,end_lst)]
+            task_descr_lst = task_lst.copy()
+            for i,task in enumerate(task_lst):
+                task_dict = ast.literal_eval(task)
+                task_dict_copy = task_dict.copy()
+                for k in task_dict.keys():
+                    if k == 'intent':
+                        continue
+                    words = task_dict[k]
+
+                    if 'room' in words: # don't need to parse the room
+                        continue
+
+                    if len(words.split()) > 1:
+                        doc = self.spacy_descr(words)
+                        dep_lst = [token.dep_ for token in doc]
+                        task_dict_copy[k] = words.split()[dep_lst.index('ROOT')]
+
+                        if dep_lst[0] == 'amod' and dep_lst[1] == 'ROOT':
+                            task_dict_copy.update({k+'_descr' : words.split()[0]})
+
+                        if dep_lst[0] == 'ROOT' and dep_lst[1] == 'acl':
+                            descr = ' '.join(words.split()[1:])
+                            # task_dict_copy.update({k+'_descr' : descr})
+                            task_dict_copy.update({k+'_descr_verb' : words.split()[1]})
+                            if 'amod' in dep_lst:
+                                task_dict_copy.update({k+'_descr_adj' : words.split()[dep_lst.index('amod')]})
+                            if 'dobj' in dep_lst:
+                                task_dict_copy.update({k+'_descr_key' : words.split()[dep_lst.index('dobj')]})
+
+                        task_descr_lst[i] = str(task_dict_copy)
+
+            parser_intent = '\n'.join(task_descr_lst)
+            # print('\n'.join(task_descr_lst))
+
+
+
+
+
+
+            # parser_intent = re.sub("''","'",parser_intent)
+            # task_lst = parser_intent.split('{')
+            # print(f'task_lst: {task_lst}')
+            # task_descr_lst = task_lst.copy()
+            # print(task_descr_lst)
+            # for i,task in enumerate(task_lst):
+            #     task_dict = json.loads(task)
+            #     for k in task_dict.keys():
+            #         if k == 'intent':
+            #             continue
+            #         words = task_dict[k]
+            #         if len(words.split()) > 1:
+            #             doc = self.spacy_descr(words)
+            #             dep_lst = [token.dep_ for token in doc]
+            #             task_dict[k] = words.split()[dep_lst.index('ROOT')]
+
+            #             if dep_lst[0] == 'amod' and dep_lst[1] == 'ROOT':
+            #                 task_dict[k+'_descr'] = words.split()[0]
+                            
+            #             if dep_lst[0] == 'ROOT' and dep_lst[1] == 'acl':
+            #                 task_dict[k+'_descr'] = words.split()[1:]
+
+            #             task_descr_lst[i] = [str(task_dict)]
+            
+            # print(f'!! task_descr_lst: {task_descr_lst}')
+                            
+
+
+              
+
             print(parser_intent)
             rospy.loginfo(B+"[Robobreizh - Dialog] Parsing Done..."+W)
             rospy.loginfo(
