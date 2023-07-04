a0faa0a1570e97d06dabeb119cdf70bd13b094fd
maelic <teoneau@gmail.com>
2023-07-04 08:53:06 +0000

    Names and locations updated

--- scripts/parserFromFile/data/common.py
+++ scripts/parserFromFile/data/common.py
@@ -1,13 +1,13 @@
 Start = ["start", "task"]
 
-Name = ["Amelia", "Angel", "Ava", "Charlie", "Charlotte", "Hunter", "Max", "Mia",
-        "Olivia", "Parker", "Sam", "Jack", "Noah", "Oliver", "Thomas", "William"]
+Name = ["Adel", "Angel", "Axel", "Charlie", "Jane", "John", "Jules", "Morgan",
+        "Paris", "Robin", "Simone"]
 
 Drink = ["Water", "Milk", "Coke", "Tonic", "Bubble Tea", "Ice Tea", "Wine", "Coffee", "Lemonade", "Iced tea", "Hot chocolate", "Juice", "Milkshake", "Tea", "Beer", "Soda",
          "Coffee", "Tea bag", "Tea", "Green tea", "Chocolate milk", "Hot chocolate", "Tomato juice", "Smoothie", "Coconut milk", "Orange juice", "Lemonade", "Fruit juice", "Cocoa"]
 
-Locations = ["House Plant", "Coat Rack", "Sofa", "Couch Table", "TV", "Side Table", "Book Shelf", "Kitchen Shelf", "Pantry Pantry", "Dinner Table",
-             "Kitchen Bin", "Fridge", "Washing Machine", "Sink", "Small Shelf", "Cupboard", "Big Shelf", "Bed", "Desk Snacks", "Show Rack", "Bin", "Office Shelf"]
+Locations = ["Entrance", "Exit", "Bed", "Night table", "Desk", "Dinning table", "Couch", "End table", "Bookcase", "Cupboard", "Storage table", "Sink", "Counter", "Dishwasher"]
+
 
 Confirmation = ["Yes", "No"]
 
