a294d84aa02930ddadec9ae0cd1cd7fe9e56dc5c
SinuoWang <1145720231@qq.com>
2023-07-05 08:09:16 +0000

    fix ask leave

--- scripts/TranscriptIntent.py
+++ scripts/TranscriptIntent.py
@@ -90,6 +90,7 @@ class Intent():
                     
                     # ----------- haven't test this yet  ----------------------------------
                     print(len(task_dict_copy))
+                    print(task_dict_copy)
                     if len(task_dict_copy) == 1: # if there is only one element (eg; no slots detected -> skip)
                         continue
                     if task_dict['intent'] == 'guide' and 'leave' in raw_request: # only ask sb to leave has "leave"
